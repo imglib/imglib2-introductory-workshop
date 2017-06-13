a3f6559b7c6adadf56f9f657da2eb542d9c2eb98
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-06-13 15:22:55 +0200

    Sandbox examples: loading and displaying images

---
+++ src/main/java/t01sandbox/SandboxIJ1.java
@@ -0,0 +1,62 @@
+package t01sandbox;
+
+import java.io.IOException;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.util.Util;
+
+/**
+ * Opening input images and displaying results using ImageJ1 wrappers.
+ *
+ * @author Tobias Pietzsch
+ */
+public class SandboxIJ1
+{
+	public static void main( final String[] args ) throws IOException
+	{
+		// show the ImageJ window
+		new ImageJ();
+
+		// open an ImageJ1 ImagePlus
+		final ImagePlus imp = IJ.openImage( "http://imagej.net/images/clown.png" );
+
+		// wrap it as an ImgLib2 img
+		final Img< ? > img = ImageJFunctions.wrap( imp );
+
+		/*
+		 * We do not know the ImgLib2 pixel type yet.
+		 *
+		 * ImageJFunctions.wrap returns
+		 *     Img< T >
+		 * where
+		 *     < T extends NumericType< T > & NativeType< T > >
+		 *
+		 * Lets just print the ImgLib2 class that we actually got:
+		 */
+		final Object type = Util.getTypeFromInterval( img );
+		System.out.println( "Pixel Type: " + type.getClass() );
+		System.out.println( "Img Type: " + img.getClass() );
+
+		// show it as an ImageJ1 (virtual) stack
+		ImageJFunctions.show( ( Img< ARGBType > ) img );
+
+
+
+
+
+
+
+		// Create an ImgLib2 Img (ArrayImg)
+		final Img< IntType > img2 = ArrayImgs.ints( 640, 480, 10 );
+
+		// show it as an ImageJ1 (virtual) stack
+		ImageJFunctions.show( img2 );
+	}
+}
---
+++ src/main/java/t01sandbox/SandboxIJ2.java
@@ -0,0 +1,61 @@
+package t01sandbox;
+
+import java.io.IOException;
+
+import net.imagej.Dataset;
+import net.imagej.ImageJ;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.util.Util;
+
+public class SandboxIJ2
+{
+	public static void main( final String[] args ) throws IOException
+	{
+		final ImageJ ij = new ImageJ();
+
+		// show the ImageJ window
+		ij.ui().showUI();
+
+		final Img< ? > img = ij.scifio().datasetIO().open( "http://imagej.net/images/clown.png" );
+
+		/*
+		 * We do not know the ImgLib2 pixel type yet.
+		 *
+		 * ImageJFunctions.wrap returns
+		 *     Img< T >
+		 * where
+		 *     < T extends NumericType< T > & NativeType< T > >
+		 *
+		 * Let's just print the ImgLib2 class that we actually got:
+		 */
+		final Object type = Util.getTypeFromInterval( img );
+		System.out.println( "Pixel Type: " + type.getClass() );
+		System.out.println( "Img Type: " + img.getClass() );
+
+
+		/*
+		 * Dataset wraps ImgPlus wraps Img.
+		 *
+		 * Let's see what the wrapped ImgLib2 Img actually is:
+		 */
+		final Img<?> wrappedImg = ( ( Dataset ) img ).getImgPlus().getImg();
+		System.out.println( "Wrapped Img Type: " + wrappedImg.getClass() );
+
+		// show the img
+		ij.ui().show( img );
+
+
+
+
+
+
+
+		// Create an ImgLib2 Img (ArrayImg)
+		final Img< IntType > img2 = ArrayImgs.ints( 640, 480, 10 );
+
+		// show it
+		ij.ui().show( img2 );
+	}
+}
