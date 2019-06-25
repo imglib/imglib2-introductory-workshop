753dbe275be257614f3accb7e6e805336e05a137
Olivier Burri <olivier.burri@gmail.com>
2019-06-25 22:18:56 +0200

    fixes incorrect method that would not return the right path String.

--- src/main/java/helpers/GetResource.java
+++ src/main/java/helpers/GetResource.java
@@ -1,11 +1,11 @@
 package helpers;
-import java.net.URLDecoder;
+import java.io.File;
 
 public class GetResource {
 
     static public String getFile(String image_name) {
         try {
-            String path =  URLDecoder.decode(GetResource.class.getResource(image_name).getFile(), "UTF-8");
+            String path = new File(GetResource.class.getResource(image_name).toURI()).getAbsolutePath();
             return path;
         } catch (Exception e) {
             System.out.println(String.format("Error Getting Image: %s", image_name, GetResource.class.getResource(image_name)));
